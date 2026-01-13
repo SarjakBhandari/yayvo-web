0168d1d5b35f310e5ba8eeb38d29f0ee8cc4cade
Sarjak Bhandari <bhandarisarjak@gmail.com>
2026-01-13 09:52:09 +0545

    created endpoints

---
+++ lib/api/endpoints.ts
@@ -0,0 +1,9 @@
+//list of backend routes
+
+export const API={
+    AUTH:{
+        REGISTER_C: '/api/auth/register/consumer',
+        REGISTER_R: '/api/auth/register/retailer',
+        LOGIN:'/api/auth/login',
+    }
+}
