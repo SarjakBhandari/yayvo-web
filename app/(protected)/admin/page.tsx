285e3b54c53d0122fa0c4f7d839548adf241b978
Sarjak Bhandari <bhandarisarjak@gmail.com>
2026-01-18 14:10:12 +0545

    dummy dashboard with logout

---
+++ app/(protected)/admin/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { handleLogout } from "@/lib/actions/auth-actions";
+
+export default function Page() {
+  const router = useRouter();
+
+  const logout = async () => {
+    try {
+      await handleLogout(); // clears cookies
+      router.push("/login"); // redirect to login page
+    } catch (err) {
+      console.error("Logout failed", err);
+    }
+  };
+
+  return (
+    <div>
+      <h1>This is admin dashboard</h1>
+      <button
+        onClick={logout}
+        className="px-4 py-2 bg-red-600 text-white rounded-md mt-4"
+      >
+        Logout
+      </button>
+    </div>
+  );
+}
