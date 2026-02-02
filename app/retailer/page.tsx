f0d4c807da482436692d856a8460c8cf2e2616f3
Sarjak Bhandari <bhandarisarjak@gmail.com>
2026-02-02 23:26:05 +0545

    made home page

--- app/retailer/page.tsx
+++ app/retailer/page.tsx
@@ -1,29 +1,14 @@
+// app/consumer/dashboard/page.tsx
 "use client";
 
+import React from "react";
 import { useRouter } from "next/navigation";
+import Sidebar from "./_components/sidebar";
 import { handleLogout } from "@/lib/actions/auth-actions";
 
+
 export default function Page() {
-  const router = useRouter();
-
-  const logout = async () => {
-    try {
-      await handleLogout(); // clears cookies
-      router.push("/login"); // redirect to login page
-    } catch (err) {
-      console.error("Logout failed", err);
-    }
-  };
-
   return (
-    <div>
-      <h1>This is retailer dashboard</h1>
-      <button
-        onClick={logout}
-        className="px-4 py-2 bg-red-600 text-white rounded-md mt-4"
-      >
-        Logout
-      </button>
-    </div>
+   <div><Sidebar/></div>
   );
 }
